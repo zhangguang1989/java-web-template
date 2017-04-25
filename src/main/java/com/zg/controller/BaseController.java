d898453ad4e161940d08372a3281e307731ff93d
guang.zhang <Zgms2016>
2017-04-25 11:03:28 +0800

    历史上的今天 缓存至redis

--- src/main/java/com/zg/controller/BaseController.java
+++ src/main/java/com/zg/controller/BaseController.java
@@ -9,15 +9,18 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.WritableResource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import java.io.*;
 import java.net.URLEncoder;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by guang.zhang on 2017/4/12.
@@ -30,10 +33,16 @@ public class BaseController {
 
     private JSONArray cachedResult = null;
 
+    private final String redisKeyPrefix = "TodayInHistory_";
+
+    @Resource
+    private RedisTemplate<Object,Object> redisTemplate;
+
     @RequestMapping({"/","/index"})
     @ResponseBody
     Object index(){
-        return "Welcome！";
+        redisTemplate.opsForValue().set("jwtTestKey","Welcome！",1, TimeUnit.MINUTES);
+        return redisTemplate.opsForValue().get("jwtTestKey");
     }
 
 
@@ -41,24 +50,22 @@ public class BaseController {
     @ResponseBody
     Object todayInHistory() throws IOException {
 
-        if (cachedResult != null){
-            return cachedResult;
-        }
+        Calendar today = Calendar.getInstance();
+        int month = today.get(Calendar.MONTH);
+        int day = today.get(Calendar.DAY_OF_MONTH);
+        String redisKey = redisKeyPrefix + String.format("%02d%02d",month,day);
 
-        File file = ResourceUtils.getFile("classpath:result.json");
-        InputStream inputStream = new FileInputStream(file);
-        String resultStr = IOUtils.toString(inputStream, "UTF-8");
-        if (!StringUtils.isEmpty(resultStr)) {
-            cachedResult = JSON.parseArray(resultStr);
+        JSONArray cachedResult = (JSONArray) redisTemplate.opsForValue().get(redisKey);
+
+        if (cachedResult != null){
             return cachedResult;
         }
 
         Map<String,Object> params = new HashMap<>();
         params.put("v","1.0");
         params.put("key",key);
-        Calendar today = Calendar.getInstance();
-        params.put("month",today.get(Calendar.MONTH));
-        params.put("day",today.get(Calendar.DAY_OF_MONTH));
+        params.put("month",month);
+        params.put("day",day);
         String urlWithParams = url + "?" + urlencode(params);
         String respStr = null;
         try {
@@ -69,9 +76,7 @@ public class BaseController {
         JSONObject resp = JSON.parseObject(respStr);
         if ((int)resp.get("error_code") == 0){
             JSONArray result = resp.getJSONArray("result");
-            OutputStream outputStream = new FileOutputStream(file);
-            IOUtils.write(result.toJSONString(), outputStream, "UTF-8");
-            cachedResult = result;
+            redisTemplate.opsForValue().set(redisKey,result,3 , TimeUnit.DAYS);
             return result;
         }
         return new ResponseResult(resp.get("reason"));
